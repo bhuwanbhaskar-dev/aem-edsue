21ec847c812ea1dfcc79799186fc5c52aee4914a
Bhuwan <bhaskarbhuwan24@gmail.com>
2026-04-17 07:06:19 +0530

    Fix: Update teaser block to match official activity file reference

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,48 +1,49 @@
+/* /blocks/teaser/teaser.js */
+
 /**
- * Adds a zoom effect to image using event listeners.
+ * Bock options are applied as classes to the block's DOM
+ * element along side the `block` and `<block-name>` classes.
  *
- * When the CTA button is hovered over, the image zooms in.
- *
- * @param {HTMLElement} block represents the block's' DOM tree
- */
+ * @param {HTMLElement} block represents the block's' DOM element/tree
+ * */
+function getOptions(block) {
+  // Get the block's classes, excluding 'block' and 'teaser'.
+  return [...block.classList].filter((c) => !['block', 'teaser'].includes(c));
+}
+
+/**
+* Adds a zoom effect to image using event listeners.
+*
+* When the CTA button is hovered over, the image zooms in.
+*
+* @param {HTMLElement} block represents the block's' DOM tree
+*/
 function addEventListeners(block) {
-  block.querySelector('.button')?.addEventListener('mouseover', () => {
-    block.querySelector('.image')?.classList.add('zoom');
+  block.querySelector('.button').addEventListener('mouseover', () => {
+    block.querySelector('.image').classList.add('zoom');
   });
 
-  block.querySelector('.button')?.addEventListener('mouseout', () => {
-    block.querySelector('.image')?.classList.remove('zoom');
+  block.querySelector('.button').addEventListener('mouseout', () => {
+    block.querySelector('.image').classList.remove('zoom');
   });
 }
 
 /**
  * Entry point to block's JavaScript.
  * Must be exported as default and accept a block's DOM element.
+ * This function is called by the project's style.js, and passed the block's element.
  *
  * @param {HTMLElement} block represents the block's' DOM element/tree
  */
 export default function decorate(block) {
-  /* This JavaScript makes minor adjustments to the block's DOM */
+/* This JavaScript makes minor adjustments to the block's DOM */
 
-  // Dress the DOM elements with semantic CSS classes so it's obvious what they are.
-  // Add a class to the first picture element to target it with CSS
-  const picture = block.querySelector('picture');
-  if (picture) {
-    picture.classList.add('image-wrapper');
-    // Use previously applied classes to target new elements
-    picture.querySelector('img')?.classList.add('image');
-  }
+  /* Common treatments for all options */
+  block.querySelector(':scope > div:last-child').classList.add('content');
+  block.querySelector('h1,h2,h3,h4,h5,h6').classList.add('title');
+  block.querySelector('img').classList.add('image');
 
-  // Mark the second/last div as the content area
-  const content = block.querySelector(':scope > div:last-child');
-  if (content) {
-    content.classList.add('content');
-  }
-
-  // Mark the first H1-H6 as a title
-  block.querySelector('h1,h2,h3,h4,h5,h6')?.classList.add('title');
-
-  // Process each paragraph and mark it as terms-and-conditions if applicable
+  // Process each paragraph and mark it as text or terms-and-conditions
   block.querySelectorAll('p').forEach((p) => {
     const innerHTML = p.innerHTML?.trim();
     if (innerHTML?.startsWith('Terms and conditions:')) {
@@ -50,6 +51,14 @@ export default function decorate(block) {
     }
   });
 
-  // Add event listeners to the block
+  /* Conditional treatments for specific options */
+  if (getOptions(block).includes('side-by-side')) {
+    /* For side-by-side teaser, add the image-wrapper a higher-level div to support CSS */
+    block.querySelector(':scope > div:first-child').classList.add('image-wrapper');
+  } else if (!getOptions(block)) {
+    /* For the default option, add the image-wrapper to the picture element to support CSS */
+    block.querySelector('picture').classList.add('image-wrapper');
+  }
+
   addEventListeners(block);
 }
